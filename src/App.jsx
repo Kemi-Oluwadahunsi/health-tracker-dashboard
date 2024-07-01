66f8f33dc0d8b2650d1215d5bcd956d585fa07c5
Oluwakemi Oluwadahunsi <kemilat50@gmail.com>
2024-07-01 11:00:07 +0800

    Update App.jsx: Add max width for all screens

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ function App() {
 
 
   return (
-    <>
+    <div className="max-w-[1600px] mx-auto">
       <div className="bg-[#F6F7F8] p-4 font-[Manrope] text-[#072635] text-[0.9rem]">
         <Header />
         <Dashboard />
       </div>
-    </>
+    </div>
   );
 }
 
